8afb933f389c6940404130b528603fcd2c68007b
Roger <rogerbambinetti@hotmail.com>
2025-11-25 19:58:10 -0300

    feat: adiciona scripts para rodar com concurrently

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,7 @@
+'use client';
+
+import { redirect } from 'next/navigation';
 
 export default function Home() {
-  return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-
-    </div>
-  );
+  redirect('/products');
 }
